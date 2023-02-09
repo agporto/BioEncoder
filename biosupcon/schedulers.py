7f83df5da59f7b783520a3986dcfccc4a0a45e1b
Arthur Porto <agporto@gmail.com>
2023-02-09 12:33:58 -0800

    init dev

--- biosupcon/schedulers.py
+++ biosupcon/schedulers.py
@@ -4,5 +4,8 @@ import torch.optim as optim
 SCHEDULERS = {
     "ReduceLROnPlateau": optim.lr_scheduler.ReduceLROnPlateau,
     "CosineAnnealingLR": optim.lr_scheduler.CosineAnnealingLR,
+    "CosineAnnealingWarmRestarts": optim.lr_scheduler.CosineAnnealingWarmRestarts,
+    "CyclicLR": optim.lr_scheduler.CyclicLR,
+    "ExponentialLR": optim.lr_scheduler.ExponentialLR,
 }
 
